0a9f442647c5b0ca5a1fd8b0ba6d068cb58d05d1
SebasG <sgarciaguerrero@safareyes.es>
2023-10-04 19:44:28 +0200

    Inicial de proyecto Safarity

---
+++ src/main/java/com/example/safarity/SafarityApplication.java
@@ -0,0 +1,13 @@
+package com.example.safarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafarityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafarityApplication.class, args);
+    }
+
+}
