d251105dd562bc82155abb8fde4519087fcf8c9c
SebasG <sgarciaguerrero@safareyes.es>
2023-10-05 17:18:25 +0200

    Evento Inicial.

--- src/main/java/com/example/safarity/model/Evento.java
+++ src/main/java/com/example/safarity/model/Evento.java
@@ -1,4 +1,17 @@
 package com.example.safarity.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Evento {
+
+
 }
