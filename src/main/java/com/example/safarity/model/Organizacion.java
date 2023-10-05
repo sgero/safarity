80539b50afbb5bafee3c04cbb689ec7e6dc656f9
CarlosPast <cpastorgamero@safareyes.es>
2023-10-05 16:52:10 +0200

    Usuario y organizacion

--- src/main/java/com/example/safarity/model/Organizacion.java
+++ src/main/java/com/example/safarity/model/Organizacion.java
@@ -1,4 +1,56 @@
 package com.example.safarity.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="organizacion",schema="safarity",catalog = "postgres")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Organizacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "cif")
+    private String cif;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "fecha_fundacion")
+    private LocalDate fechaFundacion;
+
+    @Column(name = "info")
+    private String info;
+
+    @Column(name = "sitio_web")
+    private String sitioWeb;
+
+    @Column(name = "logo")
+    private String logo;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "organizacion", fetch = FetchType.LAZY)
+    @EqualsAndHashCode.Exclude
+    private Set<Evento> eventos = new HashSet<>();
+
 }
