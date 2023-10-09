ee731e8c5d547381c8bbe145b62b5ba47ba71e8f
Eduardi <eruizgarcia@safareyes.es>
2023-10-09 18:51:07 +0200

    1er commit

--- src/main/java/com/example/safarity/repository/IAsistenteRepository.java
+++ src/main/java/com/example/safarity/repository/IAsistenteRepository.java
@@ -1,9 +1,11 @@
 package com.example.safarity.repository;
 
+import com.example.safarity.model.Asistente;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IAsistenteRepository {
+public interface IAsistenteRepository extends JpaRepository<Asistente,Integer> {
 
 
 
