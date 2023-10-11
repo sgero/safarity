0ef49e34cd856da9ae1c118e14c31b69ae41e33d
CarlosPast <cpastorgamero@safareyes.es>
2023-10-11 20:22:43 +0200

    Usuario completo, falta organizacion

--- src/main/java/com/example/safarity/service/OrganizacionService.java
+++ src/main/java/com/example/safarity/service/OrganizacionService.java
@@ -4,8 +4,6 @@ import com.example.safarity.converter.OrganizacionMapper;
 import com.example.safarity.dto.OrganizacionDTO;
 import com.example.safarity.repository.IOrganizacionRepository;
 import com.example.safarity.repository.IUsuarioRepository;
-import com.example.safarity.repository.IOrganizacionRepository;
-import com.example.safarity.repository.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +20,6 @@ public class OrganizacionService {
 
     @Autowired
     private OrganizacionMapper organizacionMapper;
-    private IUsuarioRepository usuarioRepository;
 
     public List<OrganizacionDTO> listarOrganizacion() {
         return organizacionMapper.toDTO(organizacionRepository.findAll());
