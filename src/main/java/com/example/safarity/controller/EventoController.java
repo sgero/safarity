2d2e5e59293a6e64ec52b414f09fff7e5ca5f008
SebasG <sgarciaguerrero@safareyes.es>
2023-10-19 19:04:35 +0200

    Controller y Services EVENTO

--- src/main/java/com/example/safarity/controller/EventoController.java
+++ src/main/java/com/example/safarity/controller/EventoController.java
@@ -1,8 +1,10 @@
 package com.example.safarity.controller;
 
+import com.example.safarity.converter.EventoMapper;
 import com.example.safarity.dto.EventoDTO;
 import com.example.safarity.repository.IEventoRepository;
 import com.example.safarity.service.EventoService;
+import jdk.jfr.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,9 @@ public class EventoController {
     @Autowired
     private IEventoRepository eventoRepository;
 
+    @Autowired
+    private EventoMapper eventoMapper;
+
     @GetMapping(value = "/listar")
     public List<EventoDTO> listarEventos(){
         return eventoService.listarEventos();
@@ -29,11 +34,15 @@ public class EventoController {
     }
 
     @PutMapping(value="/modificar")
-
+    public EventoDTO modificarEvento(@RequestBody EventoDTO eventoDTO) {
+        return eventoMapper.toDTO(eventoService.modificarEvento(eventoDTO));
+    }
 
 
     @DeleteMapping(value="/eliminar")
-
+    public String eliminarEvento(@RequestBody EventoDTO eventoDTO){
+        return eventoService.eliminarEvento(eventoDTO);
+    }
 
 
     //    @DeleteMapping(value= "/{id}")
