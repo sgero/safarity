4e86bc3bab859c5b3f2c925e6c8f8846406c8db6
SebasG <sgarciaguerrero@safareyes.es>
2023-10-19 18:01:48 +0200

    Controller y Services EVENTO

--- src/main/java/com/example/safarity/controller/EventoController.java
+++ src/main/java/com/example/safarity/controller/EventoController.java
@@ -1,6 +1,7 @@
 package com.example.safarity.controller;
 
 import com.example.safarity.dto.EventoDTO;
+import com.example.safarity.repository.IEventoRepository;
 import com.example.safarity.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,13 +15,29 @@ public class EventoController {
     @Autowired
     private EventoService eventoService;
 
+    @Autowired
+    private IEventoRepository eventoRepository;
+
     @GetMapping(value = "/listar")
     public List<EventoDTO> listarEventos(){
         return eventoService.listarEventos();
     }
 
-    @DeleteMapping(value= "/{id}")
-    public void eliminarEvento(@PathVariable Long id) {
-        eventoService.eliminarEvento(id);
+    @PostMapping(value = "/crear")
+    public EventoDTO crearEvento(@RequestBody EventoDTO eventoDTO) {
+        return eventoService.crearEvento(eventoDTO);
     }
+
+    @PutMapping(value="/modificar")
+
+
+
+    @DeleteMapping(value="/eliminar")
+
+
+
+    //    @DeleteMapping(value= "/{id}")
+//    public void eliminarEvento(@PathVariable Long id) {
+//        eventoService.eliminarEvento(id);
+//    }
 }
--- src/main/java/com/example/safarity/service/EventoService.java
+++ src/main/java/com/example/safarity/service/EventoService.java
@@ -48,15 +48,57 @@ public class EventoService {
         return listEventos;
     }
 
-    //Eliminar un evento
-    public void eliminarEvento(Long id) {
-        eventoRepository.deleteById(id);
-    }
+//    //Eliminar un evento
+//    public void eliminarEvento(Long id) {
+//        eventoRepository.deleteById(id);
+//    }
 
     //Método para crear y modificar que se llame GUARDAR
-    public void guardarEvento(EventoDTO eventoDTO) {
-        Evento evento = new Evento();
-        evento.setId(eventoDTO.getId());
+    //Me lo cargo porque lo voy a hacer con el MAPPER
+//    public void guardarEvento(EventoDTO eventoDTO) {
+//        Evento evento = new Evento();
+//        evento.setId(eventoDTO.getId());
+//        evento.setNombre(eventoDTO.getNombre());
+//        evento.setDescripcion(eventoDTO.getDescripcion());
+//        evento.setDireccion(eventoDTO.getDireccion());
+//        evento.setImagen(eventoDTO.getImagen());
+//        evento.setAforo(eventoDTO.getAforo());
+//        evento.setTotalAsistentes(eventoDTO.getTotalAsistentes());
+//        evento.setTipoEvento(eventoDTO.getTipoEvento());
+//        evento.setTipoPago(eventoDTO.getTipoPago());
+//        evento.setFechaInicio(eventoDTO.getFechaInicio());
+//        evento.setFechaFin(eventoDTO.getFechaFin());
+//        evento.setFechaLanzamiento(eventoDTO.getFechaLanzamiento());
+//        evento.setFechaVentaDisponible(eventoDTO.getFechaVentaDisponible());
+//        evento.setEntradasVendidas(eventoDTO.getEntradasVendidas());
+//
+//        eventoRepository.save(evento);
+//    }
+
+
+    //Crear y modificar en un mismo método????
+//    public void guardarEvento(EventoDTO eventoDTO) {
+//        Evento evento = eventoMapper.toEntity(eventoDTO);
+//        eventoRepository.save(evento);
+//
+//    }
+
+
+    //Crear un EVENTO
+    public EventoDTO crearEvento(EventoDTO eventoDTO) {
+        return eventoMapper.toDTO(eventoRepository.save(eventoMapper.toEntity(eventoDTO)));
+    }
+
+    //Modificar un EVENTO
+    public Evento modificarEvento(EventoDTO eventoDTO) {
+        Evento evento = eventoRepository.findById(eventoDTO.getId()).orElse(null);
+
+        if (evento == null) {
+            return null;
+        }
+        else
+        {
+
         evento.setNombre(eventoDTO.getNombre());
         evento.setDescripcion(eventoDTO.getDescripcion());
         evento.setDireccion(eventoDTO.getDireccion());
@@ -71,6 +113,22 @@ public class EventoService {
         evento.setFechaVentaDisponible(eventoDTO.getFechaVentaDisponible());
         evento.setEntradasVendidas(eventoDTO.getEntradasVendidas());
 
-        eventoRepository.save(evento);
+        Evento eventoModificado = eventoRepository.save(evento);
+
+        return eventoModificado;
+        }
+    }
+
+    public String eliminarEvento (EventoDTO eventoDTO){
+        Evento eventoEliminar = eventoRepository.findById(eventoDTO.getId()).orElse(null);
+        if (eventoEliminar != null){
+            eventoRepository.delete(eventoEliminar);
+            return "Evento eliminado correctamente";
+            }else {
+            return "Nose ha podido eliminar el evento";
+        }
+
+
+
     }
 }
