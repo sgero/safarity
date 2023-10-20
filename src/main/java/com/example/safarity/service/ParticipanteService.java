1a31cd354045422c155681081bd579f390979476
SebasG <sgarciaguerrero@safareyes.es>
2023-10-20 17:17:14 +0200

    Participante Repository y Services

--- src/main/java/com/example/safarity/service/ParticipanteService.java
+++ src/main/java/com/example/safarity/service/ParticipanteService.java
@@ -1,5 +1,65 @@
 package com.example.safarity.service;
 
+import com.example.safarity.converter.ParticipanteMapper;
+import com.example.safarity.dto.EventoDTO;
+import com.example.safarity.dto.ParticipanteDTO;
+import com.example.safarity.model.Evento;
+import com.example.safarity.model.Participante;
+import com.example.safarity.repository.IParticipanteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ParticipanteService {
 
+    @Autowired
+    private IParticipanteRepository participanteRepository;
+
+    @Autowired
+    private ParticipanteMapper participanteMapper;
+
+    public List<ParticipanteDTO> listarParticipantes() {
+        List<ParticipanteDTO> listParticipantes = new ArrayList<>();
+        participanteRepository.findAll().forEach(p->listParticipantes.add(participanteMapper.toDTO(p)));
+        return listParticipantes;
+    }
+
+    public ParticipanteDTO crearParticipante(ParticipanteDTO participanteDTO) {
+        return participanteMapper.toDTO(participanteRepository.save(participanteMapper.toEntity(participanteDTO)));
+    }
+
+    public Participante modificarParticipante(ParticipanteDTO participanteDTO) {
+        Participante participante = participanteRepository.findById(participanteDTO.getId()).orElse(null);
+
+        if (participante == null) {
+            return null;
+        }
+        else
+        {
+
+            participante.setNombre(participanteDTO.getNombre());
+            //MAS CAMPOS HAY QUE INCLUIR
+
+
+            Participante participanteModificado = participanteRepository.save(participante);
+
+            return participanteModificado;
+        }
+    }
+
+    public String eliminarParticipante (ParticipanteDTO participanteDTO){
+        Participante participanteEliminar = participanteRepository.findById(participanteDTO.getId()).orElse(null);
+        if (participanteEliminar != null){
+            participanteRepository.delete(participanteEliminar);
+            return "Participante eliminado correctamente";
+        }else {
+            return "Nose ha podido eliminar el participante";
+        }
+
+
+
+    }
 }
