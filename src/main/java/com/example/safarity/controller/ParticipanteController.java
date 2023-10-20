666a575d67c2f538aba525c41389218f2d4685dc
SebasG <sgarciaguerrero@safareyes.es>
2023-10-20 17:19:37 +0200

    Participante Controller

--- src/main/java/com/example/safarity/controller/ParticipanteController.java
+++ src/main/java/com/example/safarity/controller/ParticipanteController.java
@@ -1,4 +1,50 @@
 package com.example.safarity.controller;
 
+import com.example.safarity.converter.EventoMapper;
+import com.example.safarity.converter.ParticipanteMapper;
+import com.example.safarity.dto.EventoDTO;
+import com.example.safarity.dto.ParticipanteDTO;
+import com.example.safarity.repository.IEventoRepository;
+import com.example.safarity.repository.IParticipanteRepository;
+import com.example.safarity.service.EventoService;
+import com.example.safarity.service.ParticipanteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/participante")
 public class ParticipanteController {
+
+
+    @Autowired
+    private ParticipanteService participanteService;
+
+    @Autowired
+    private IParticipanteRepository participanteRepository;
+
+    @Autowired
+    private ParticipanteMapper participanteMapper;
+    @GetMapping(value = "/listar")
+    public List<ParticipanteDTO> listarParticipantes(){
+        return participanteService.listarParticipantes();
+    }
+
+    @PostMapping(value = "/crear")
+    public ParticipanteDTO crearParticipante(@RequestBody ParticipanteDTO participanteDTO) {
+        return participanteService.crearParticipante(participanteDTO);
+    }
+
+    @PutMapping(value="/modificar")
+    public ParticipanteDTO modificarEvento(@RequestBody ParticipanteDTO participanteDTO) {
+        return participanteMapper.toDTO(participanteService.modificarParticipante(participanteDTO));
+    }
+
+
+    @DeleteMapping(value="/eliminar")
+    public String eliminarEvento(@RequestBody ParticipanteDTO participanteDTO){
+        return participanteService.eliminarParticipante(participanteDTO);
+    }
+
 }
