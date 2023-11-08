af2dec6f658678cf3ea1b4e56644bee8f8e9ec20
Sebastian <sgarciaguerrero@safareyes.es>
2023-11-08 12:20:37 +0100

    @CrossOrigin(origins = "http://localhost:4200/")

--- src/main/java/com/example/safarity/controller/ParticipanteController.java
+++ src/main/java/com/example/safarity/controller/ParticipanteController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 @RequestMapping("/participante")
 public class ParticipanteController {
 
