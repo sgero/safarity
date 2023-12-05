3db4e6bfb2e7caae2960d325295593c848ebcc1a
SebasG <sgarciaguerrero@safareyes.es>
2023-12-05 19:41:31 +0100

    MAIL SENDER Completo (comprobar que llega el mail)

---
+++ src/main/java/com/example/safarity/controller/MensajeController.java
@@ -0,0 +1,39 @@
+package com.example.safarity.controller;
+
+import com.example.safarity.dto.MensajeDTO;
+import com.example.safarity.service.MensajeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class MensajeController {
+
+
+
+    @Autowired
+    private MensajeService emailService; // Debes tener un servicio para enviar correos electrónicos
+
+
+    @PostMapping("/enviar-mensaje")
+    public ResponseEntity<String> enviarMensaje(@RequestBody MensajeDTO mensaje) {
+        try {
+            // Lógica para enviar el mensaje por correo electrónico
+            emailService.enviarCorreo("safarityTeam@gmail.com", "Nuevo mensaje de contacto", construirCuerpoMensaje(mensaje));
+            return new ResponseEntity<>("Mensaje enviado correctamente", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error al enviar el mensaje", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private String construirCuerpoMensaje(MensajeDTO mensaje) {
+        // Construye el cuerpo del mensaje a partir de los datos del formulario
+        return "Nombre: " + mensaje.getNombre() + "\nCorreo electrónico: " + mensaje.getEmail() + "\nMensaje: " + mensaje.getMensaje();
+    }
+}
